633d3f2394ef04b5ba6dc81cb509238dc119248d
Fajar Muhammad <fajaribrahm@gmail.com>
2023-11-15 16:38:29 +0700

    loops dan condition + NGC2

---
+++ package1/module1.py
@@ -0,0 +1,2 @@
+def sayHello(userName, age):
+    print("hello, ", userName, age)
---
+++ sayHello.py
@@ -0,0 +1,7 @@
+import package1.module1 as mod1 
+#cara import 1, mod1 adalah nama alias untuk memperpendek pemanggilan module
+from package1.module1 import sayHello, globalVar
+#cara import 2, import function secara spesifik
+
+
+mod1.sayHello("fajra", 14)
